52cbbd6ed35e23e0d66e8934f875a2fc6bfd82b8
Marc Mills <mdmills1990@gmail.com>
2018-08-21 20:01:59 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -17,7 +17,7 @@ end
 def take_a_number(array, name)
   if array.length == 0
     array.unshift(name)
-    puts "Welcome #{name}. You are number #{array.length} in line."
+    puts "Welcome, #{name}. You are number #{array.length} in line."
   end
 end
 
